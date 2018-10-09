0f33587e883a7c9555a9e4b29fe92306d007417a
Alexander Frosell <sachafrosell@gmail.com>
2018-10-09 20:58:42 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,13 @@ class School
     @roster
   end
 
- 
+   def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
 
   def grade(number)
     @roster[number]
